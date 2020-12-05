fa14c45db870690359897d55105a04323907174d
Max Einhorn <maxeinhorn@gmail.com>
2020-12-05 13:57:52 -0800

    Files cleaned up--moving on to postgres

--- Database/Profile.js
+++
@@ -1,9 +0,0 @@
-// var mongoose = require('mongoose');
-
-// var ProfileSchema = new mondoose.Schema({
-//   email: {type:String, default:''},
-//   password: {type:String, default:''},
-//   timestamp: {type:Date, default: Date.now}
-// })
-
-// module.exports = mongoose.model('ProfileSchema', ProfileSchema);
