079160569ef80a6189c9d036dbbd6ea1b24bb535
Max Einhorn <maxeinhorn@gmail.com>
2020-12-05 12:51:11 -0800

    Replaced Mongo Seeding

--- Database/mongoSeeding.js
+++ Database/mongoSeeding.js
@@ -1,85 +1,46 @@
-const mongoose = require('mongoose');
-const Listing = require('./dbschema.js')
+const faker = require('faker');
+const fs = require('fs');
 
+const writeUsers = fs.createWriteStream('listings.csv');
+writeUsers.write('id,photo1,photo2,photo3,photo4,photo5,photo6\n', 'utf8');
 
-var seed = () => {
-  var docsToInsert = [];
-  var documentGenerator = (id) => {
-    return {
-      listingId: id,
-      name:'Sonja',
-      photo1:'https://loremflickr.com/320/240/treehouse,tree',
-      photo2:'https://loremflickr.com/320/240/treehouse,forest',
-      photo3:'https://loremflickr.com/320/240/treehouse,green',
-      photo4:'https://loremflickr.com/320/240/treehouse,brown',
-      photo5:'https://loremflickr.com/320/240/treehouse,light',
-      photo6:'hhttps://loremflickr.com/320/240/treehouse,dark',
+function writeTenMillionUsers(writer, encoding, callback) {
+  let i = 10000000;
+  let id = 0;
+  let housingOptions = ['treehouse', 'igloo', 'castle', 'hut', 'shack', 'bungalow', 'highrise'];
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      let index = Math.floor((housingOptions.length * Math.random()));
+      let photo1 ='https://loremflickr.com/320/240/big,' + housingOptions[index];
+      let photo2 ='https://loremflickr.com/320/240/small,' + housingOptions[index];
+      let photo3 ='https://loremflickr.com/320/240/light,' + housingOptions[index];
+      let photo4 ='https://loremflickr.com/320/240/dark,' + housingOptions[index];
+      let photo5 ='https://loremflickr.com/320/240/wide,' + housingOptions[index];
+      let photo6 ='https://loremflickr.com/320/240/narrow' + housingOptions[index];
+      const data = `${id},${photo1},${photo2},${photo3},${photo4},${photo5},${photo6}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+// see if we should continue, or wait
+// don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+// had to stop early!
+// write some more once it drains
+      writer.once('drain', write);
     }
   }
-  for (var i = 0; i < 1000000; i++) {
-    docToInsert = documentGenerator(i);
-    docsToInsert.push(docToInsert);
-    if (i % 10000 === 0) {
-      Listing.insertMany(docsToInsert)
-      .then(function() {
-        console.log('Data seeded successfully');
-      })
-      .catch(function(err) {
-        console.log(err);
-      })
-      docsToInsert = [];
-    }
-  }
-  // Listing.insertMany(docsToInsert, (err, data) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return;
-  //   } else {
-  //     console.log('Data seeded successfully');
-  //     // mongoose.connection.close();
-  //   }
-  // })
-  console.log('Data seeded successfully');
-  // mongoose.connection.close();
+write()
 }
 
-seed();
+writeTenMillionUsers(writeUsers, 'utf-8', () => {
+  writeUsers.end();
+});
 
-// SEEDING SCRIPT
-// node ./Database/mongoSeeding.js
-
-// EXPORT TO CSC
-// mongoexport --collection=listings --db=airbnb --out=listings.csv
-
-
-
-// const mongoose = require('mongoose');
-// const Listing = require('./dbschema.js')
-
-
-
-// const listingOne = new Listing({
-//   listingId: 3,
-//   name:'Sonja',
-//   photo1:'https://airbnb-photo-gallery.s3-us-west-1.amazonaws.com/SonjaHouse/photo1.jpg',
-//   photo2:'https://airbnb-photo-gallery.s3-us-west-1.amazonaws.com/SonjaHouse/photo2.jpg',
-//   photo3:'https://airbnb-photo-gallery.s3-us-west-1.amazonaws.com/SonjaHouse/photo3.jpg',
-//   photo4:'https://airbnb-photo-gallery.s3-us-west-1.amazonaws.com/SonjaHouse/photo4.jpg',
-//   photo5:'https://airbnb-photo-gallery.s3-us-west-1.amazonaws.com/SonjaHouse/photo5.jpg',
-//   photo6:'https://airbnb-photo-gallery.s3-us-west-1.amazonaws.com/SonjaHouse/photo6.jpg',
-// })
-
-// listingOne.save();
-
-// for (var i = 0; i <= 1000; i++) {
-//   docToInsert = documentGenerator(i);
-//   docsToInsert.push(docToInsert);
-//   if (i % 100 === 0) {
-//     Listing.insertMany(docsToInsert)
-//     docsToInsert = [];
-//   }
-//   if (i === 1000) {
-//     console.log('Data seeded successfully');
-//     mongoose.connection.close();
-//   }
-// }
+// ongoimport --db airbnb --collection listings --type csv --headerline --ignoreBlanks --file ./listings.csv
+//
